034d438291f5378b9748a1eaf798d4163219dc8f
Takács Kornél <Takcs Kornl@DESKTOP-ODLCDHM>
2022-09-20 09:43:22 +0200

    Login error added

---
+++ Talkin/LoginError.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Talkin.Assets.Themes.UI_Element_Styles
+{
+    /// <summary>
+    /// Interaction logic for NoInternetErrorWindow.xaml
+    /// </summary>
+    public partial class NoInternetErrorWindow : Window
+    {
+        public NoInternetErrorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
