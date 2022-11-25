9e66aea5955aa818cef276477f7b4b778f3d2981
benedek461 <rave01385@gmail.com>
2022-11-25 10:27:28 +0100

    Todo és egyéb módosítások hozzáadva.

--- Backend_Talking/Models/Conversation.cs
+++ Backend_Talking/Models/Conversation.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
 
-        public List<User> Partitioners { get; set; }
-        public List<ConversationUser> ConversationUsers { get; set; }
+        public List<User>? Partitioners { get; set; }
+        public List<ConversationUser>? ConversationUsers { get; set; }
 
-        public List<Message> Messages { get; set; }
+        public List<Message>? Messages { get; set; }
     }
 }
--- Backend_Talking/Models/Message.cs
+++ Backend_Talking/Models/Message.cs
@@ -4,10 +4,10 @@
     {
         public int Id { get; set; }
         public int SenderId { get; set; }
-        public string Text { get; set; }
+        public string? Text { get; set; }
         public DateTime CreatedAt { get; set; }
 
         public int ConversationId { get; set; }
-        public Conversation Conversation { get; set; }
+        public Conversation? Conversation { get; set; }
     }
 }
